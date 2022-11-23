d0a6274e76e7af248a4686089d7eaf93a7e14513
SanjanaGodolkar <sanjana_g@gwu.edu>
2022-11-23 13:03:28 -0500

    Update Team6_FinalProject.py

--- Team6_FinalProject.py
+++ Team6_FinalProject.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
+import seaborn as sns
+
 
 
 
@@ -63,7 +65,20 @@ crimedf["DayofWeek"]=crimedf["START_DATE"].dt.day_name()
 
 # Crimdedf: barchart of frequency by district, barchart of frequency by month, barchart of frequency by hour
 
+#Countplot for hour
+sns.countplot(x ='Hour', data = crimedf,palette = "Set2").set(title='Countplot for frequncy of crime during differet hours')
 
+#Countplot for month
+sns.countplot(x ='Month', data = crimedf,palette = "Set2").set(title='Countplot for frequncy of crime during differet months')
+
+#Countplot for day of week
+sns.countplot(x ='DayofWeek', data = crimedf,palette = "Set2").set(title='Countplot for frequncy of crime during days of week')
+
+#pie chart for day of week
+plt.pie(crimedf['DayofWeek'].value_counts().values, labels=crimedf['DayofWeek'].value_counts().index, colors=sns.color_palette('colorblind'), autopct='%.2f')
+plt.title("Pie chart showing crime during different days of the week",bbox={'facecolor':'1.0', 'pad':5})
+plt.legend(bbox_to_anchor=(2,0.5), loc="center right")
+plt.show()
 
 #%%
 #Modeling 
