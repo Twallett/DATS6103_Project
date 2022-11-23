5b13d3b1d0b03187eb23f74146744662f0d42728
Twallett <twallett@gwu.edu>
2022-11-23 10:42:16 -0500

    init

--- Team6_FinalProject.py
+++ Team6_FinalProject.py
@@ -1,2 +1,37 @@
 #%%
+#Importing modules 
+
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+#%%
+#Importing datasets 
+
+crime = pd.read_csv('Crime_Incidents_in_2022.csv')
+stop = pd.read_csv('Stop_Data.csv')
+
+
+# %%
+#Selecting columns of interest 
+
+
+
+
+#%% 
+#Cleaning the data set 
+
+
+
+#%%
+#EDA
+
+
+#%%
+#Modeling 
+
+
+
+#Conclusions 
